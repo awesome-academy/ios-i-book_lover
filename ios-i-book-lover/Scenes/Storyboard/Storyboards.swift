ee3c1d4782289337994f890c14524a1aca15df84
nkhanh44 <nguyen.nam.khanh@sun-asterisk.com>
2019-04-03 16:38:45 +0700

    Apple MVC Structure

---
+++ ios-i-book-lover/Scenes/Storyboard/Storyboards.swift
@@ -0,0 +1,14 @@
+//
+//  Storyboards.swift
+//  ios-i-book-lover
+//
+//  Created by nguyen.nam.khanh on 4/3/19.
+//  Copyright © 2019 nguyen.nam.khanh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Storyboards {
+    static var main = UIStoryboard(name: "Main", bundle: nil)
+}
---
+++ ios-i-book-lover/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-i-book-lover
+//
+//  Created by nguyen.nam.khanh on 4/3/19.
+//  Copyright © 2019 nguyen.nam.khanh. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
