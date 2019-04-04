0ba00491243406a76b8adc8262270d401dccc342
nkhanh44 <nguyen.nam.khanh@sun-asterisk.com>
2019-04-04 08:19:34 +0700

    Task_9941:Tab bar
     with '#' will be ignored, and an empty message aborts the commit.

---
+++ ios-i-book-lover/Scenes/Explore/ExploreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreViewController.swift
+//  BookLover
+//
+//  Created by nguyen.nam.khanh on 4/4/19.
+//  Copyright © 2019 nguyen.nam.khanh. All rights reserved.
+//
+
+import UIKit
+
+final class ExploreViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- ios-i-book-lover/Scenes/Storyboard/Storyboards.swift
+++ ios-i-book-lover/Scenes/Storyboard/Storyboards.swift
@@ -11,4 +11,8 @@ import UIKit
 
 enum Storyboards {
     static var main = UIStoryboard(name: "Main", bundle: nil)
+    static var home = UIStoryboard(name: "Home", bundle: nil)
+    static var profile = UIStoryboard(name: "Profile", bundle: nil)
+    static var explore = UIStoryboard(name: "Explore", bundle: nil)
+
 }
